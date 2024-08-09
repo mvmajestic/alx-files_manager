ac0319cef1cf9d7520d22bc52cec00d7cb170e8a
Vuyisanani Mkutyukelwa <vuyisanani@ymail.com>
2024-08-09 10:01:32 -0700

    Task 0: Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,44 @@
+import { createClient } from 'redis';
+import { promisify } from 'util';
+
+// class that defines methods for commonly used redis cmds
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to server: ${error}`);
+    });
+  }
+
+  // will check connection status and report
+  isAlive() {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // will aquire value for given key from redis server
+  async get(key) {
+    const redisGet = promisify(this.client.get).bind(this.client);
+    const value = await redisGet(key);
+    return value;
+  }
+
+  // sets key value pair to redis server
+  async set(key, value, time) {
+    const redisSet = promisify(this.client.set).bind(this.client);
+    await redisSet(key, value);
+    await this.client.expire(key, time);
+  }
+
+  // deletes key vale pair from redis server
+  async del(key) {
+    const redisDel = promisify(this.client.del).bind(this.client);
+    await redisDel(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
